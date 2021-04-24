b335e2bd373b1a380dfe1b3f1b697f83fbdef56e
Arsenii Netrebenko <arsenii.netrebenko@gmail.com>
2021-04-24 15:41:27 +0300

    📦 NEW: Click listener

---
+++ 10_Animations/script.js
@@ -0,0 +1,7 @@
+const sabers = document.querySelector('.sabers-wrapper')
+console.log(sabers)
+if (true) {
+    sabers.addEventListener('click', function () {
+        sabers.classList.toggle('selected')
+    })
+}
